7a72e59e37ed99457fc17dd2f0f57880b17bb2d1
Luis Felipe Hau <luisfelipehau@gmail.com>
2024-03-08 14:51:01 -0300

    study modules third-party

---
+++ study_modules_third_party.py
@@ -0,0 +1,13 @@
+# pip3 install requests==2.31.0
+
+# Importa a biblioteca requests, um módulo de terceiros, para realizar solicitações HTTP
+import requests
+
+# Define a URL para fazer a solicitação HTTP
+url = "https://www.example.com"
+
+# Envia uma solicitação HTTP GET para a URL especificada e armazena a resposta
+response = requests.get(url)
+
+# Exibe o status code da resposta HTTP
+print(f"Solicitação HTTP para {url} retornou o status {response.status_code}")
