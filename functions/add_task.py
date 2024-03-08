266e8f6d9411b6673e5df3f8f0614edf5c93a229
Luis Felipe Hau <luisfelipehau@gmail.com>
2024-03-08 11:16:00 -0300

    add task

--- functions/add_task.py
+++ functions/add_task.py
@@ -1,4 +1,15 @@
 def add_task(tasks, task_name, task_description):
+    """
+    Adiciona uma nova tarefa à lista de tarefas.
+
+    Args:
+        tasks (list): Lista de tarefas existentes.
+        task_name (str): Nome da nova tarefa a ser adicionada.
+        task_description (str): Descrição da nova tarefa.
+
+    Returns:
+        list: Lista de tarefas atualizada após a adição da nova tarefa.
+    """
     # Estrutura da tarefa: keys -> name, description e status
     task = {"tarefa": task_name, "status": False, "descrição": task_description}
     tasks.append(task)
--- manager.py
+++ manager.py
@@ -1,3 +1,5 @@
+from functions.add_task import add_task
+
 tasks = []
 
 while True:
@@ -12,23 +14,8 @@ while True:
     choice = input("Digite sua escolha: ")
     if choice == "1":
         task_name = input("Qual é o nome da tarefa que deseja adicionar? ")
-
-        while True:
-            try:
-                # Tenta converter o input para inteiro
-                task_number = int(task_name)
-                print("Você digitou um número, por favor, insira um nome válido.")
-                task_name = input(
-                    "Qual é o nome da tarefa que deseja adicionar? "
-                )  # Atualiza o valor de task_name
-            except ValueError:
-                # Se ocorrer uma ValueError (quando a conversão para int falha), o nome da tarefa é válido
-                print("Tarefa adicionada:", task_name)
-                break
-            except Exception as e:
-                # Qualquer outra exceção que ocorrer será tratada aqui
-                print("Ocorreu um erro:", e)
-                break
+        task_description = input("Qual será a descrição da tarefa?")
+        tasks = add_task(tasks, task_name, task_description)
 
     elif choice == "6":
         break
